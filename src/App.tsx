fb24e89fe65612b79198122081a031cf3c31e77e
Rebecca Tay <rebeccatay92@gmail.com>
2023-09-04 16:45:45 +0800

    feat: init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Root from './pages/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (<Root />),
+  },
+  {
+    path: '/cafes',
+    element: (<div>cafes</div>)
+  },
+  {
+    path: '/cafes/:id',
+    element: (<div>edit cafe</div>)
+  },
+  {
+    path: '/cafes/new',
+    element: (<div>add cafe</div>)
+  },
+  {
+    path: '/employees',
+    element: (<div>employees</div>)
+  },
+  {
+    path: '/employees/:id',
+    element: (<div>edit employee</div>)
+  },
+  {
+    path: '/employees/new',
+    element: (<div>add employee</div>)
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
